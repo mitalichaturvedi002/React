f9f2e63acbb209a8ed8c86e723fb8e80839c8c49
Mitali Chaturvedi <chaturvedimitali002@gmail.com>
2026-04-22 23:23:44 +0530

    first commit

--- src/Topic-2 Props/Card.jsx
+++ src/Topic-2 Props/Card.jsx
@@ -6,20 +6,28 @@ const Card = (props) => {
   const {username, age, team, skill, jersy,address} = props?.player;
   const {name,city} = address?.state;
 
+const Card = ({ username = "Chombu Singh", age, skill, team, jersy }) => {
   return (
     <>
+    <div
+      style={{
+        border: "1px solid black",
+        width: "300px",
+        padding: "20px 30px",
+        marginTop: "20px",
+      }}
+    >
       <h4>Username: {username}</h4>
       <h4>Age: {age}</h4>
       <h4>Team: {team}</h4>
       <h4>Skill: {skill[2]}</h4>
+      <h4>Skill: {skill}</h4>
       <h4>Jersy: {jersy}</h4>
       <h4>City: {city}</h4>
       <h4>State: {name}</h4>
     </>
+    </div>
   );
 };
 
 export default Card;
-
-
-
--- src/Topic-2 Props/Parent.jsx
+++ src/Topic-2 Props/Parent.jsx
@@ -1,8 +1,8 @@
-import Card from "./Card";
 
 const Parent = () => {
 
     const player = {
+    const player1 = {
         username: "Rohit Sharma",
         age: 38,
         team: "MI",
@@ -15,17 +15,32 @@ const Parent = () => {
                 city: "Mumbai"
             },
         }
+        skill: "batting",        
+    }    
+
+    const player2 = {
+        username: "Virat Kohli",
+        age: 36,
+        team: "RCB",
+        jersy: 18,
+        skill: "batting",        
     }
 
+    const player3 = {
+        username: "Mahendra Singh Dhoni",
+        age: 43,
+        team: "CSK",
+        jersy: 7,
+        skill: "batting",        
+    }
+        
     return <div className="parent-component">
             
             {/* <Card username="Virat Kohli" age={36} team="RCB" skill="batting" jersy={18} /> */}
 
             <Card player={player} />
+            <Card {...player1} />
+            <Card {...player2} />
+            <Card {...player3} />
     </div>
 }
-
-
-
-
-export default Parent;
